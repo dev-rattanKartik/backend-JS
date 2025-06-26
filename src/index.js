7e02bf634e5361b9395f4b73ec920faf3c99f87c
kartik.rattan <rattankartikmai@gmail.com>
2025-06-26 15:43:01 +0530

    routes defined, import & declaration. controller and debugging

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
@@ -8,12 +9,14 @@ dotenv.config({
 const port = process.env.PORT || 4000;
 
 connectDb()
-.then((port) =>{
+.then(() =>{
     app.on('error', (error) => {
         console.log("ERR: ", error);
         throw error;
     })
-    app.listen(port, `Server is listening on the port ${port}`);
+    app.listen(port, () => {
+        console.log(`Server is listening on the port ${port}`);
+    });
 })
 .catch((err) => {
     console.log("MONGO DB connection failed", err);
