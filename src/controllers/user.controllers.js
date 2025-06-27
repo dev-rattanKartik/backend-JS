5cb3f8a946b58139950b5c03deda18be18a6103b
kartik.rattan <rattankartikmai@gmail.com>
2025-06-27 19:22:08 +0530

    bugs fixes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -17,12 +17,12 @@ const registerUser = asyncHandler(async (req, res) => {
   let { fullname, email, username, password } = req.body;
 
   if (
-    [fullname, email, username, password].some((field) => field?.trim === "")
+    [fullname, email, username, password].some((field) => field?.trim() === "")
   ) {
     throw new ApiError(400, "All fields are required");
   }
 
-  const existedUser = User.findOne({
+  const existedUser = await User.findOne({
     $or: [{ username }, { email }],
   });
 
@@ -31,7 +31,13 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 
   const avatarLocalPath = req.files?.avatar[0]?.path;
-  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  )
+    coverImageLocalPath = req.files.coverImage[0].path;
 
   if (!avatarLocalPath) {
     throw new ApiError(400, "Avatar file is required");
@@ -50,6 +56,7 @@ const registerUser = asyncHandler(async (req, res) => {
     username: username.toLowerCase(),
     avatar: avatar.url,
     coverImage: coverImage?.url || "",
+    password,
   });
 
   const isUserExisted = await User.findById(user._id).select(
