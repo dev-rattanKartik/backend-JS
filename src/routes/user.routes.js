704e5bc622b67bfc3e4785703204ac5f2612db8e
kartik.rattan <rattankartikmai@gmail.com>
2025-06-27 16:46:11 +0530

    controllers user and upload avatar, coverImage to cloudinary

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controllers.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser);
+router.route("/register").post(
+  //inject the middleware to upload given fields before going to registerUser
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export default router;
